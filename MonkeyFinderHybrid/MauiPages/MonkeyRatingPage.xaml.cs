3e13958e181a94b0e6413c7391b059b620fdf975
Johan Sanchez <johan.sanchez@ghtcorp.com>
2025-03-30 17:51:40 -0500

    RatingPage

--- MonkeyFinderHybrid/MauiPages/MonkeyRatingPage.xaml.cs
+++ MonkeyFinderHybrid/MauiPages/MonkeyRatingPage.xaml.cs
@@ -6,4 +6,10 @@ public partial class MonkeyRatingPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(System.Object sender, System.EventArgs e)
+    {
+		Debug.WriteLine($"Monkey rating: {rating.Value}");
+		Navigation.PopModalAsync();
+    }
 }
